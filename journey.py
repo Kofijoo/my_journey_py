90b97a80c64c462cfe4ef2985b50d17b60778e51
Kofijoo <joshuaagyekum21@gmail.com>
2025-04-21 16:15:44 +0200

    Unpack Tuples

--- journey.py
+++ journey.py
@@ -107,4 +107,209 @@ Which one of these is a tuple?
 thistuple = ('apple', 'banana', 'cherry')
 thistuple = ['apple', 'banana', 'cherry']
 thistuple = {'apple', 'banana', 'cherry'}
-"""
+
+Python - Access Tuple Items
+Access Tuple Items
+You can access tuple items by referring to the index number, inside square brackets:
+
+Example
+Print the second item in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+Note: The first item has index 0.
+
+Negative Indexing
+Negative indexing means start from the end.
+
+-1 refers to the last item, -2 refers to the second last item etc.
+
+Example
+Print the last item of the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[-1])
+Range of Indexes
+You can specify a range of indexes by specifying where to start and where to end the range.
+
+When specifying a range, the return value will be a new tuple with the specified items.
+
+Example
+Return the third, fourth, and fifth item:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+Note: The search will start at index 2 (included) and end at index 5 (not included).
+
+Remember that the first item has index 0.
+
+By leaving out the start value, the range will start at the first item:
+
+Example
+This example returns the items from the beginning to, but NOT included, "kiwi":
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+By leaving out the end value, the range will go on to the end of the tuple:
+
+Example
+This example returns the items from "cherry" and to the end:
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+Range of Negative Indexes
+Specify negative indexes if you want to start the search from the end of the tuple:
+
+Example
+This example returns the items from index -4 (included) to index -1 (excluded)
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+Check if Item Exists
+To determine if a specified item is present in a tuple use the in keyword:
+
+Example
+Check if "apple" is present in the tuple:
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+Exercise
+?
+You can access tuple items by referring to the index number, but what is the index number of the first item?
+
+
+-1
+0
+1
+
+Python - Update Tuples
+Tuples are unchangeable, meaning that you cannot change, add, or remove items once the tuple is created.
+
+But there are some workarounds.
+
+Change Tuple Values
+Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+
+But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+
+Example
+Convert the tuple into a list to be able to change it:
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+
+print(x)
+Add Items
+Since tuples are immutable, they do not have a built-in append() method, but there are other ways to add items to a tuple.
+
+1. Convert into a list: Just like the workaround for changing a tuple, you can convert it into a list, add your item(s), and convert it back into a tuple.
+
+Example
+Convert the tuple into a list, add "orange", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+2. Add tuple to a tuple. You are allowed to add tuples to tuples, so if you want to add one item, (or many), create a new tuple with the item(s), and add it to the existing tuple:
+
+Example
+Create a new tuple with the value "orange", and add that tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+Note: When creating a tuple with only one item, remember to include a comma after the item, otherwise it will not be identified as a tuple.
+
+Remove Items
+Note: You cannot remove items in a tuple.
+
+Tuples are unchangeable, so you cannot remove items from it, but you can use the same workaround as we used for changing and adding tuple items:
+
+Example
+Convert the tuple into a list, remove "apple", and convert it back into a tuple:
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+Or you can delete the tuple completely:
+
+Example
+The del keyword can delete the tuple completely:
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print(thistuple) #this will raise an error because the tuple no longer exists
+Exercise
+?
+You cannot change the items of a tuple, but there are workarounds. Which of the following suggestion will work?
+
+
+Convert tuple into a list, change item, convert back into a tuple.
+Convert tuple into a set, change item, convert back into a tuple.
+Convert tuple into a dictionary, change item, convert back into a tuple.
+Python - Unpack Tuples
+Unpacking a Tuple
+When we create a tuple, we normally assign values to it. This is called "packing" a tuple:
+
+Example
+Packing a tuple:
+
+fruits = ("apple", "banana", "cherry")
+But, in Python, we are also allowed to extract the values back into variables. This is called "unpacking":
+
+Example
+Unpacking a tuple:
+
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+Note: The number of variables must match the number of values in the tuple, if not, you must use an asterisk to collect the remaining values as a list.
+
+Using Asterisk*
+If the number of variables is less than the number of values, you can add an * to the variable name and the values will be assigned to the variable as a list:
+
+Example
+Assign the rest of the values as a list called "red":
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+(green, yellow, *red) = fruits
+
+print(green)
+print(yellow)
+print(red)
+If the asterisk is added to another variable name than the last, Python will assign values to the variable until the number of values left matches the number of variables left.
+
+Example
+Add a list of values the "tropic" variable:
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+
+(green, *tropic, red) = fruits
+
+print(green)
+print(tropic)
+print(red)
+Exercise
+?
+Consider the following code:
+fruits = ('apple', 'banana', 'cherry')
+(x, y, z) = fruits
+print(y)
+What will be the value of y?
+
+
+apple
+banana
+cherry
